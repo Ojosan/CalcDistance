a52620d43e1df10917b3dda54d916dabed11651d
Ojosan <koshka.slytherin@gmail.com>
2021-03-13 18:36:46 -0500

    Distance Traveled

---
+++ distanceCalc/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace distanceCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //color
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            Console.Write("enter a time (in hours): ");
+            var time =Console.ReadLine();
+            Console.Write("enter a speed (mph) : ");
+            var speed = Console.ReadLine();
+
+            //math
+            double result = Convert.ToDouble(time) * Convert.ToDouble(speed);
+
+            //new colors
+            Console.ResetColor();
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //results
+            Console.WriteLine("you will travel " + result + " miles");
+
+            //close program
+            Console.WriteLine("press any key to close.");
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
